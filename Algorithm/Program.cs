0783a5dc8fd0f0c8587c20cbf5643e8096fdcc6b
동윤 김 <ut_community@naver.com>
2023-04-18 15:47:23 +0900

    docs : Algorithm Explaination
    알고리즘에 대한 설명

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -6,6 +6,11 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
+            /* 알고리즘 (Algorithm)
+             * 
+             * 문제를 해결하기 위해 정해진 진행절차나 방법
+             * 컴퓨터에서 알고리즘은 어떠한 행동을 하기 위해서 만들어진 프로그램 명령어의 집합 */
+
             Console.WriteLine("Hello World!");
         }
     }
