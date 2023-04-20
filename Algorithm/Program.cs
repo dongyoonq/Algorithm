9893b3375f8effb45052b54364db92ad0b8d0b02
dongyoonq <ut_community@naver.com>
2023-04-20 23:10:23 +0900

    feat & refector : Add Average Method , and Code Refectoring, Comment
    모든 Int형 자료형을 가진 자료구조의 평균을 구해주는 Average 메서드 구현 및
    코드 리팩토링, 주석 추가

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -50,83 +50,125 @@ namespace Algorithm
                 // 가장 높은 차수의 계수와 나머지 모든 항을 제거하고표기
                 // 알고리즘의 대략적인 효율을 파악할 수 있는 수단.
             }
+
+            // 사용자 LinkedList 반복자 사용
             int[] arr = { 5, 4, 6, 8, 3, 2 };
             MyLinkedList<int> list = new MyLinkedList<int>(arr);
 
+            // MyLinkedList Enumerator 생성
             IEnumerator<int> enumerator = list.GetEnumerator();
             
+            // 반복자(Enumerator)을 사용한 foreach 구문 사용.
             foreach (int item in list)
                 Console.Write($"{item} ");
             
             Console.WriteLine();
 
+            // 반복자(Enumerator)을 사용한 while 구문 사용.
             while (enumerator.MoveNext())
                 Console.Write($"{enumerator.Current} ");
 
             Console.WriteLine();
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine();
 
-            /////////////////////////////////////////////////////////////////
+            // 사용자 List 반복자 사용
             int[] arr2 = { 1, 6, 8, 43, 21, 12 };
             MyList<int> list2 = new MyList<int>(arr2);
 
+            // MyList Enumerator 생성
             IEnumerator<int> enumerator1 = list2.GetEnumerator();
 
+            // 반복, 인덱스를 통한 for 구문 사용.
             for (int i = 0; i < list2.Count; i++)
                 Console.Write($"{list2[i]} ");
 
             Console.WriteLine();
 
+            // 반복자(Enumerator)을 사용한 foreach 구문 사용.
             foreach (int item in list2)
                 Console.Write($"{item} ");
 
             Console.WriteLine();
-            
-            while(enumerator1.MoveNext())
+
+            // 반복자(Enumerator)을 사용한 while 구문 사용.
+            while (enumerator1.MoveNext())
                 Console.Write($"{enumerator1.Current} ");
 
             Console.WriteLine();
             /////////////////////////////////////////////////////////////////////////////////////////////
             Console.WriteLine();
 
+            // 배열(Array), 와 선형리스트(List) 둘 모두 정렬 가능한 사용자 Sort 사용
             MyList<int> newlist = new MyList<int>() { 3, 4, 5, 76, 7, 98 };
+
+            // 선형 리스트(List) 오름차순 정렬
             Sort(newlist, AscendingSort);
             foreach(int item in newlist)
                 Console.Write($"{item} ");
 
             Console.WriteLine();
 
+            // 선형 리스트(List) 내림차순 정렬
             Sort(newlist, DescendingSort);
             foreach (int item in newlist)
                 Console.Write($"{item} ");
 
             Console.WriteLine();
 
+            // 배열(Array) 오름차순 정렬
             Sort(arr, AscendingSort);
             foreach (int item in arr)
                 Console.Write($"{item} ");
 
             Console.WriteLine();
 
+            // 배열(Array) 내림차순 정렬
             Sort(arr, DescendingSort);
             foreach (int item in arr)
                 Console.Write($"{item} ");
 
             Console.WriteLine();
-
-            Sort(arr2, AscendingSort);
-            foreach (int item in arr2)
-                Console.Write($"{item} ");
-
+            /////////////////////////////////////////////////////////////////////////////////////////////
             Console.WriteLine();
 
-            Sort(arr2, DescendingSort);
-            foreach (int item in arr2)
-                Console.Write($"{item} ");
+            // int 자료구조면 다 평균을 구할수 있는 Average(자료구조) 사용자 Average 사용
+
+            // MyLinkedList<int> 평균 구하기
+            Console.WriteLine(Average(list));
+
+            // MyList<int> 평균 구하기
+            Console.WriteLine(Average(list2));
+
+            // int 배열 평균 구하기
+            Console.WriteLine(Average(arr));
+
+            // LinkedList<int> 평균 구하기
+            LinkedList<int> lls = new LinkedList<int>(arr2);
+            Console.WriteLine(Average(lls));
+
+            // List<int> 평균 구하기
+            List<int> ls = new List<int>(arr);
+            Console.WriteLine(Average(ls));
+
+            // Stack<int> 평균 구하기
+            Stack<int> stack = new Stack<int>(arr);
+            Console.WriteLine(Average(stack));
+
+            // Queue<int> 평균 구하기
+            Queue<int> queue = new Queue<int>(arr);
+            Console.WriteLine(Average(queue));
         }
 
-
+        /// 비교 해주는 델리게이트 지정자 선언
         public delegate int Compare<T>(T left, T right);
 
+        /// <summary>
+        /// 지정자를 이용해 모든 배열, 리스트를 정렬해주는 메서드
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="compare"></param>
         public static void Sort<T>(IList<T> list, Compare<T> compare)
         {
             for(int i = 0; i < list.Count; i++)
@@ -143,6 +185,13 @@ namespace Algorithm
             }    
         }
         
+        /// <summary>
+        /// 오름차순을 위한 델리게이트 메서드
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static int AscendingSort<T>(T left, T right) where T : IComparable<T>
         {
             if (left.CompareTo(right) > 0)
@@ -153,6 +202,13 @@ namespace Algorithm
                 return 0;
         }
 
+        /// <summary>
+        /// 내림차순을 위한 델리게이트 메서드
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static int DescendingSort<T>(T left, T right) where T : IComparable<T>
         {
             if (left.CompareTo(right) > 0)
@@ -163,6 +219,19 @@ namespace Algorithm
                 return 0;
         }
 
-
+        /// <summary>
+        /// IEnumerable<int> 한 모든 자료구조, 자료형을 받아 평균을 구해주는 메서드
+        /// int 자료구조의 평균을 구하는 메서드
+        /// </summary>
+        /// <param name="enumerable"></param>
+        /// <returns></returns>
+        public static double Average(IEnumerable<int> enumerable)
+        {
+            int sum = 0, count = 0;
+            IEnumerator<int> enumerator = enumerable.GetEnumerator();
+            while (enumerator.MoveNext())
+            { sum += enumerator.Current; count++; }
+            return sum / (double)count;
+        }
     }
 }
