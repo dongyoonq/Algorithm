46f81486b55b8f6fca617e616f5962fbc3cea4f1
dongyoonq <ut_community@naver.com>
2023-04-22 00:43:53 +0900

    feat : BracketCheck
    스택을 이용한 괄호검사 기능 추가

--- Algorithm/Stack, Queue/Homework/BracketCheck.cs
+++ Algorithm/Stack, Queue/Homework/BracketCheck.cs
@@ -7,11 +7,13 @@ namespace Algorithm
     internal class BracketCheck
     {
         private string input;
+        private bool isTrueBracket = true;
         Stack<char> bracket = new Stack<char> ();
 
         public void Start()
         {
             Input();
+            Render();
         }
 
         private void Input()
@@ -27,34 +29,50 @@ namespace Algorithm
                 switch(input[i])
                 {
                     case '(':
-                        bracket.Push(input[i]);
-                        break;
-                    case ')':
-                        bracket.Push(input[i]);
-                        break;
                     case '[':
-                        bracket.Push(input[i]);
-                        break;
-                    case ']':
-                        bracket.Push(input[i]);
-                        break;
                     case '{':
                         bracket.Push(input[i]);
                         break;
+                    case ')':
+                        if (IsEmpty(bracket))
+                            break;
+                        if (bracket.Peek() == '(') 
+                            bracket.Pop();
+                        break;
+                    case ']':
+                        if (IsEmpty(bracket))
+                            break;
+                        if (bracket.Peek() == '[') 
+                            bracket.Pop();
+                        break;
                     case '}':
-                        bracket.Push(input[i]);
+                        if (IsEmpty(bracket))
+                            break;
+                        if (bracket.Peek() == '{') 
+                            bracket.Pop();
                         break;
                     default:
                         break;
                 }
             }
 
-            Check(bracket);
+            if (bracket.Count != 0)
+                isTrueBracket = false;
         }
 
-        private void Check(Stack<char> bracket)
+        private bool IsEmpty(Stack<char> bracket)
         {
+            if (bracket.Count == 0)
+            { isTrueBracket = false; return true; }
+            return false;
+        }
 
+        private void Render()
+        {
+            if(isTrueBracket)
+                Console.WriteLine("정상적인 괄호입니다");
+            else
+                Console.WriteLine("비정상적인 괄호입니다");
         }
     }
 }
