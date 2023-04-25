c3f2eac7ae2c336a0c38a4a769eee19b124eeb35
동윤 김 <ut_community@naver.com>
2023-04-25 21:51:30 +0900

    feat : Traversal DFS, BFS Add
    순회 알고리즘, DFS/BFS 추가

---
+++ Algorithm/Tree/Traversal/BFS.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class BFS
+    {
+        int[,] graph = new int[10, 10];
+        bool[] visited = new bool[10];
+
+        private void Search(int num)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(num);
+            visited[num] = true;
+
+            while(queue.Count > 0)
+            {
+                int p = queue.Dequeue();
+                Array.Sort(graph);
+
+                for (int i = 0; i < graph.GetLength(p); i++)
+                {
+                    int v = graph[p,i];
+                    if(!visited[v])
+                    {
+                        visited[v] = true;
+                        queue.Enqueue(v);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithm/Tree/Traversal/DFS.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class DFS
+    {
+        int[,] graph = new int[10,10];
+        bool[] visited = new bool[10];
+
+        public void Start()
+        {
+            int i = 1;
+            // PreOrderTraversal
+            PreOrderTravel(i);
+            Console.WriteLine();
+            // InOrderTraversal
+            InOrderTravel(i);
+            Console.WriteLine();
+            // PostOrderTraversal
+            PostOrderTravel(i);
+            Console.WriteLine();
+        }
+
+        private void Search(int num)
+        {
+            visited[num] = true;
+
+            Array.Sort(graph);
+            for(int i = 0; i < graph.GetLength(num); i++)
+            {
+                int v = graph[num,i];
+                if(!visited[v])
+                    Search(v);
+            }
+        }
+
+        // 전위 순회
+        private void PreOrderTravel(int num)
+        {
+            if (num > 7) return;
+
+            Console.Write($"{num} ");       // 자신
+            PreOrderTravel(num * 2);        // 왼쪽 자식
+            PreOrderTravel(num * 2 + 1);    // 오른쪽 자식
+        }
+
+        // 중위 순회
+        private void InOrderTravel(int num)
+        {
+            if (num > 7) return;
+
+            InOrderTravel(num * 2);         // 왼쪽 자식
+            Console.Write($"{num} ");       // 자신
+            InOrderTravel(num * 2 + 1);     // 오른쪽 자식
+        }
+
+        // 후위 순회
+        private void PostOrderTravel(int num)
+        {
+            if (num > 7) return;
+
+            PostOrderTravel(num * 2);
+            PostOrderTravel(num * 2 + 1);
+            Console.Write($"{num} ");
+        }
+    }
+}
