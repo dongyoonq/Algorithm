c5b8e1d47dc89cf7690413cc338dbe90bbf25104
dongyoonq <ut_community@naver.com>
2023-05-03 00:10:19 +0900

    feat : My Search - SequentialSearch, BinarySearch Add
    순차탐색, 이진탐색 구현 추가

---
+++ Algorithm/Algorithm/Search/MySearching/BinarySearch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Lucene.Net.Documents.Field;
+
+namespace Algorithm
+{
+    /// <summary>
+    /// 정렬되어있어야만 사용가능하다.
+    /// </summary>
+    internal class BinarySearch
+    {
+        public bool Search<T>(IList<T> list, T item, int startIndex, int count) where T : IComparable<T>
+        {
+            if (startIndex < 0 || startIndex + count > list.Count)
+                throw new IndexOutOfRangeException();
+            if (count < 0)
+                throw new ArgumentOutOfRangeException();
+
+            int low = startIndex;
+            int high = startIndex + count - 1;
+
+            while (low <= high)
+            {
+                int middle = low + (high - low) / 2;
+                int compare = list[middle].CompareTo(item);
+
+                // 배열의 중간값이 요소보다 작다면 큰부분요소 부터 다시 탐색
+                if (compare < 0)
+                    low = middle + 1;
+                // 배열의 중간값이 요소보다 크다면 작은부분요소 부터 다시 탐색
+                else if (compare > 0)
+                    high = middle - 1;
+                // 배열의 중간값이 요소랑 같으면 True반환
+                else
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Algorithm/Algorithm/Search/MySearching/SequentialSearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class SequentialSearch
+    {
+        public bool Search<T>(IList<T> list, T item) where T : IComparable<T>
+        {
+            // 요소랑 일치하는것이 리스트에 있다면 True반환
+            for(int i = 0; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(item) == 0)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
